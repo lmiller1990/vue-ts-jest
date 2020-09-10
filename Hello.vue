836678300779ee66f1c242c9e65f4fa80671c13b
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-09-10 14:25:15 +1000

    init

---
+++ Hello.vue
@@ -0,0 +1,10 @@
+<template>
+  <button @click="inc">{{ count }}</button>
+</template>
+
+<script setup="props, { emit }" lang="ts">
+import { ref } from 'vue'
+
+export const count = ref(0)
+export const inc = () => count.value++
+</script>
---
+++ README.md
@@ -0,0 +1,1 @@
+Coming soon. Experimental Vue 3 Jest transformer using ts-jest.
