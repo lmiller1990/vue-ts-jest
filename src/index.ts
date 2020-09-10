66964ad006ffa7abc79337dd54e105115199d4f3
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-09-11 00:27:57 +1000

    mode: function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { parse, compileScript, compileTemplate, BindingMetadata, SFCScriptBlock } from '@vue/compiler-sfc'
-import { transpile } from 'typescript'
+// import { transpile } from 'typescript'
 import { TsJestTransformer } from 'ts-jest/dist/ts-jest-transformer';
 import type { TransformedSource, Transformer, TransformOptions } from '@jest/transform';
 import type { Config } from '@jest/types';
@@ -7,12 +7,12 @@ import type { Config } from '@jest/types';
 // Example in vue-loader: https://github.com/vuejs/vue-loader/commit/fb09c8b1755086c4e0627d0e83035e8ef53ed5c3
 // Script Setup RFC: https://github.com/vuejs/rfcs/blob/sfc-improvements/active-rfcs/0000-sfc-script-setup.md#transform-api
 
-function transpileToCommonJS(code: string) {
-  return transpile(code, {
-    module: 1, // commonjs
-    target: 1  // es5
-  })
-}
+// function transpileToCommonJS(code: string) {
+//   return transpile(code, {
+//     module: 1, // commonjs
+//     target: 1  // es5
+//   })
+// }
 
 class VueTsJestTransformer extends TsJestTransformer implements Transformer {
   process(input: string, filePath: Config.Path, jestConfig: Config.ProjectConfig, transformOptions?: TransformOptions): TransformedSource | string {
@@ -29,10 +29,11 @@ class VueTsJestTransformer extends TsJestTransformer implements Transformer {
         filename: descriptor.filename,
         source: descriptor.template!.content,
         compilerOptions: {
-          bindingMetadata: bindings
+          bindingMetadata: bindings,
+          mode: 'function'
         }
       })
-      const templateCode = transpileToCommonJS(template.code)
+      const templateCode = template.code
       // this is fairly dirty
       if (templateCode.includes('exports.render = render;')) {
         output += templateCode + ';exports.default = {...exports.default, render};'
