c80abe46f7697f6fc66a711018d20298d6ae876c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-09-10 15:38:51 +1000

    add some useful links

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { parse, compileScript, compileTemplate, BindingMetadata, SFCScriptBlock } from '@vue/compiler-sfc'
 import { transpile } from 'typescript'
 
-// Script Setup RFC: https://github.com/vuejs/vue-loader/commit/fb09c8b1755086c4e0627d0e83035e8ef53ed5c3
+// Example in vue-loader: https://github.com/vuejs/vue-loader/commit/fb09c8b1755086c4e0627d0e83035e8ef53ed5c3
+// Script Setup RFC: https://github.com/vuejs/rfcs/blob/sfc-improvements/active-rfcs/0000-sfc-script-setup.md#transform-api
 
 function transpileToCommonJS(code: string) {
   return transpile(code, {
